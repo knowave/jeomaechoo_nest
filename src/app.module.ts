065f386609e02c142b67bb2754c0753256d32c61
knowave <kwaktaemin98@gmail.com>
2024-06-13 20:37:45 +0900

    add mysql module, menu module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MysqlModule } from './database/mysql.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-  imports: [],
+  imports: [MysqlModule, MenuModule],
   controllers: [AppController],
   providers: [AppService],
 })
