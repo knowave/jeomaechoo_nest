a3f4b827a526fc653ab4ead58de1576ceb98e8ec
knowave <kwaktaemin98@gmail.com>
2024-06-13 21:04:35 +0900

    add s3 module

--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { MenuController } from './menu.controller';
 import { MenuRepository } from './menu.repository';
+import { S3Module } from 'src/s3/s3.module';
 
 @Module({
-  controllers: [MenuController],
+  controllers: [MenuController, S3Module],
   providers: [MenuService, MenuRepository],
 })
 export class MenuModule {}
