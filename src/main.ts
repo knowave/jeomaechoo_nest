f34a1316ef3083be98476d1c866b272002deb0d1
knowave <kwaktaemin98@gmail.com>
2024-06-13 21:25:39 +0900

    swagger and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Jeomaechoo API')
+    .setDescription('The Dinner menu API Description')
+    .setVersion('1.0')
+    .addTag('Jeomaechoo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(8080);
 }
 bootstrap();
