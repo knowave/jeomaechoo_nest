6a83de73bee665e07d533eec2ec42cd27d4ca7ce
knowave <kwaktaemin98@gmail.com>
2024-06-13 21:04:11 +0900

    exception error message

---
+++ src/menu/menu.error.ts
@@ -0,0 +1,9 @@
+export const INVALID_INPUT_DATA = {
+  code: 'INVALID_INPUT_DATA',
+  message: '이름과 이미지를 확인해주세요.',
+};
+
+export const MENU_NOT_FOUND = {
+  code: 'MENU_NOT_FOUND',
+  message: '메뉴를 찾을 수 없습니다.',
+};
