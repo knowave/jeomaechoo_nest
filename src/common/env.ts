bc24623a271e6c098067e398eb23b76c178bf0d4
knowave <kwaktaemin98@gmail.com>
2024-06-15 13:26:10 +0900

    AWS env 추가

--- src/common/env.ts
+++ src/common/env.ts
@@ -6,3 +6,8 @@ export const DATABASE_PORT = +process.env.DATABASE_PORT;
 export const DATABASE_USER = process.env.DATABASE_USER;
 export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 export const DATABASE_NAME = process.env.DATABASE_NAME;
+
+// AWS S3 Environment Variables
+export const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
