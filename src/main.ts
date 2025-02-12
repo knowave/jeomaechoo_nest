85d671be7b3d1497a17aa9d5206243d1aa41fb8d
knowave <kwaktaemin98@gmail.com>
2025-02-12 15:20:17 +0900

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { IS_NODE_ENV_PROD, ORIGIN } from './common/env';
+import { swaggerConfig } from './common/config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Jeomaechoo API')
-    .setDescription('The Dinner menu API Description')
-    .setVersion('1.0')
-    .addTag('Jeomaechoo')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  if (!IS_NODE_ENV_PROD) console.log('is not prod');
+  if (!IS_NODE_ENV_PROD) swaggerConfig(app);
 
   app.enableCors({
     origin: ORIGIN,
